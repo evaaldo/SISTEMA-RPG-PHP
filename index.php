6cce01c7c5b21ee7dc3dc648f8d800dfe84550f3
evaaldo <evaaldormj@gmail.com>
2023-05-14 02:01:02 -0300

    Feat

--- index.php
+++ index.php
@@ -11,9 +11,10 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
+    <h1 class="titulo_rpg">Taverna</h1>
     <h1>Jogadores</h1>
     <a href="cadastro.php">Criar um jogador</a>
     <a href="login.php">Acessar um jogador</a>
